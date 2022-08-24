183d26a300f7a2a0d53dfc75404a31ac0137e26d
lucca <luccaopereira@outlook.com>
2022-08-24 19:11:00 -0300

    viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,3 +4,4 @@ let job = "na_procura"
 let age = 21
 let country = "holanda" 
 console.log({name,adress,job,age,country})
+console.log(`Ola ${name}! Somos aqui da agencia de turismo Viagem dos sonhos. Estamos te escrevendo este email, pois acabamos de confimar aas compras das passagens aereas para ${country}. Confimar pra gente alguns dados? A sua idade é ${age}, voce é ${job} e o seu email é ${adress}? Ficamos no aguardo. Muito obrigado e boa viagem! :D `)
