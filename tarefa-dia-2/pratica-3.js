6a758aac5df9e848e9c3fc40da34f22c406ce158
lucca <luccaopereira@outlook.com>
2022-08-25 18:18:18 -0300

    turingcode

--- tarefa-dia-2/pratica-3.js
+++ tarefa-dia-2/pratica-3.js
@@ -2,10 +2,10 @@ let turing = "ABCDBDHEKSJLFYHAZDNJCLAAWJBHTZVEKVMIFKHFKCEHEKLUTBQLFYHAZDNJCLAAWJ
 let tamanho = turing.length
 let linhas = 6110
  
-if (linhas < 6110 ) {
-  console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas.")
+if (linhas <= 6110 ) {
+  console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D.")
 } else {
-  console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D.")  
+  console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas.")  
 }
 
 
